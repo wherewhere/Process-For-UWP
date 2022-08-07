c8eceffe3ad1491d61da572451f5546a1034d90b
wherewhere <qq2518766683@outlook.com>
2022-08-08 00:37:21 +0800

    更新了一些内容

--- Demo/ProcessForUWP.Demo.Delegate/Program.cs
+++ Demo/ProcessForUWP.Demo.Delegate/Program.cs
@@ -1,18 +1,5 @@
 ﻿using ProcessForUWP.Desktop.Helpers;
-using System.Threading;
 
-namespace AdbApkInstallerUWP.Delegate
-{
-    internal class Program
-    {
-        private static void Main(string[] args)
-        {
-            Communication.InitializeAppServiceConnection();
-
-            while (true)
-            {
-                Thread.Sleep(100);
-            }
-        }
-    }
-}
+Communication.InitializeAppServiceConnection();
+EventWaitHandle WaitHandle = new AutoResetEvent(false);
+_ = WaitHandle.WaitOne();
